1519c4de5bb142742d53923a26243373e96000be
njfactor <njfactor@github.com>
2014-07-19 10:10:36 +0530

    first

---
+++ js/funcs_pagetwo.js
@@ -0,0 +1,80 @@
+
+$(document).on("pagebeforeshow","#pagetwo",function(event,data)
+{
+		$("#billDetails").text('');
+		$('<div class="ui-block-a " style="width:40%;"  ><h3 style="text-align:left;">Product</h3></div><div class="ui-block-b " style="width:10%;" ><h3 style="text-align:right; ">Qty</h3></div><div class="ui-block-c " style="width:25%;" ><h3 style="text-align:right; ">Price</h3></div><div class="ui-block-d " style="width:25%;"><h3 style="text-align:right; ">S.Total</h3></div></div>').appendTo("#billDetails");
+		
+		$("#grandTotalPagetwo").text(total_price.toFixed(2));
+		
+		for(i = 0;i<cart_top;i++)
+		{
+			addBillToDisplay(i);
+			total_Discount+=(cart[i].mrp - cart[i].mallPrice)*cart[i].qty;
+		}
+		$("#totalDiscountPagetwo").text(total_Discount.toFixed(2));
+});
+
+
+function addBillToDisplay(index)
+{
+	var $bill=$('<div class="ui-block-a" id="bd" style="text-align:left;width:40%;">' + cart[index].pdName + '</div><div class="ui-block-b" id="bd" style="text-align:right;width:10%;">' + cart[index].qty + '</div><div class="ui-block-c" id="bd" style="text-align:right;width:25%;">Rs. ' + cart[index].mallPrice.toFixed(2) + '</div><div class="ui-block-d" id="bd" style="text-align:right;width:25%;">Rs. ' + cart[index].subTotal.toFixed(2) + '</div>').appendTo(document.getElementById('billDetails'));
+}
+
+
+function sendToBiller()
+{
+               event.preventDefault();
+
+               cartForServer = new cartS(cartID,cart_top,cart,total_price);
+               var finurl=server_url;
+               dat= JSON.stringify(cartForServer);
+               
+			   if(confirm('To submit the cart please provide the following QR Code or this CartID: '+ cartForServer.cartID+' at the billing counter.' ))
+				{
+                $.ajax
+               ({
+                       type: "POST",
+                       url: finurl,
+                       data:dat,
+                       headers: { "Accept":"application/json"},
+
+                       success: function (data) 
+                       {
+                           
+						 // alert('post sent');
+							if(data[0]=='I'){							
+							alert("Invalid post to server");
+							return;							
+							}
+								
+ 							var urlForBiller = getQueryStringForBiller(server_url,cartForServer.cartID);
+						//  alert(urlForBiller);
+						//	alert('Please show the QR Code or Cart ID at billing counter.\nCartID: '+cartForServer.cartID);
+							app.encode(urlForBiller); 
+						//	alert('Thank you for shopping with Virtual Cart.');
+                       },
+                       error: function ()
+                       {
+                               alert('Sorry! there seems to be a problem with our servers. Please try paying after some time.');
+                       }        
+               });
+			   }
+}
+
+function getQueryStringForBiller (url,cartId)
+{	
+return  url+"?method=getBillToPrint&cartID="+cartId;	
+}
+
+function showQRCodeAlert()
+{
+	confirm('Provide the following CartID: '+ cartForServer.cartID + ' at the billing counter.');
+}
+
+function cartS( cartID,  itemQuantity, prodarr, total)
+{
+	this.cartID=cartID;
+	this.itemQuantity=itemQuantity;
+	this.prodarr=prodarr;
+	this.total=total;
+}
